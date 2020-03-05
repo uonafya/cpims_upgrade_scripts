7afe688fdbe9f45f3349424dd913872b3b3ed596
rosaga <richardjohnzranger@gmail.com>
2020-03-05 16:51:30 +0300

    delete reg_org_unit

--- auth_user/alter_auth_user.sql
+++ auth_user/alter_auth_user.sql
@@ -3,7 +3,7 @@ CREATE TABLE tmp_auth_user AS (SELECT * FROM auth_user);
 CREATE TABLE tmp_auth_user AS (select id, ROW_NUMBER () OVER (ORDER BY id) + 2500 as new_id from auth_user where id > 1);
 
 -- reset the ids to the new ids created in temp
-UPDATE auth_user SET id=tmp_auth_user.new_id FROM tmp_auth_user WHERE auth_user.id=tmp_auth_user.id and auth_user.id < 1000;
+UPDATE auth_user SET id=tmp_auth_user.new_id FROM tmp_auth_user WHERE auth_user.id=tmp_auth_user.id ;
 
 -- DELETE FROM auth_user WHERE id > 1 and  < 1001;
 
