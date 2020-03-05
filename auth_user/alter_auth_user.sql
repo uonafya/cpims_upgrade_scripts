bfbdc600be4e2f094733b5065f0a4b7098718673
Benzer Bett <benzerbett@gmail.com>
2020-03-05 16:13:36 +0300

    conflix

--- auth_user/alter_auth_user.sql
+++ auth_user/alter_auth_user.sql
@@ -1,10 +1,6 @@
 -- create temporary backup table
 -- CREATE TABLE tmp_auth_user AS (SELECT * FROM auth_user);
-<<<<<<< HEAD
 CREATE TABLE tmp_auth_user AS (select id, ROW_NUMBER () OVER (ORDER BY id) + 1000 as new_id from auth_user where id > 1);
-=======
-CREATE TABLE tmp_auth_user AS (select id, ROW_NUMBER () OVER (ORDER BY id) + 1000 as new_id from auth_user where id > 1 and id < 2200);
->>>>>>> 579d39e465caea71476f462a759aab434acc6a58
 
 -- reset the ids to the new ids created in temp
 UPDATE auth_user SET id=tmp_auth_user.new_id FROM tmp_auth_user WHERE auth_user.id=tmp_auth_user.id;
