0b4993feb064b41fb0e11fe732d475374feaf735
rosaga <richardjohnzranger@gmail.com>
2020-03-06 10:22:41 +0300

    finalized

--- append Scripts/order.sql
+++ append Scripts/order.sql
@@ -506,6 +506,44 @@ WHERE item_id NOT IN (SELECT item_id FROM list_general) and item_description NOT
 
 
 
+----ovc_bursaryinfo
+
+INSERT INTO ovc_bursaryinfo (bursary_id, bursary_type, disbursement_date, amount, "year", term, timestamp_created, is_void, is_active, sync_id, created_by, person_id)
+(SELECT bursary_id, bursary_type, disbursement_date, amount, "year", term, timestamp_created, is_void, is_active, sync_id, created_by, person_id  FROM
+    dblink('dbname=cpims_dcs user=postgres password=admin1234', 'select bursary_id, bursary_type, disbursement_date, amount, "year", term, timestamp_created, is_void, is_active, sync_id, created_by, person_id FROM ovc_bursaryinfo')
+AS t(bursary_id uuid, bursary_type varchar, disbursement_date date, amount varchar, "year" varchar, term varchar, timestamp_created timestamptz, is_void bool, is_active bool, sync_id uuid, created_by int4, person_id int4)
+);
+---working fine
+
+----ovc_adverseevents_followup
+
+INSERT INTO ovc_adverseevents_followup (adverse_condition_id , adverse_condition_description, attendance_type, referral_type , adverse_event_date, created_by , timestamp_created , is_void, sync_id , person_id, placement_id_id  )
+(SELECT adverse_condition_id , adverse_condition_description, attendance_type, referral_type , adverse_event_date, created_by , timestamp_created , is_void, sync_id , person_id, placement_id_id FROM
+    dblink('dbname=cpims_dcs', 'select adverse_condition_id , adverse_condition_description, attendance_type, referral_type , adverse_event_date, created_by , timestamp_created , is_void, sync_id , person_id, placement_id_id FROM ovc_adverseevents_followup')
+AS t(adverse_condition_id uuid, adverse_condition_description varchar, attendance_type varchar , referral_type varchar , adverse_event_date date, created_by int, timestamp_created date, is_void bool, sync_id uuid, person_id int, placement_id_id uuid )
+
+);
+---working fine
+
+
+
+-----ovc_adverseevents_other_followup
+
+INSERT INTO ovc_adverseevents_other_followup (adverse_condition , timestamp_created , is_void , sync_id , adverse_condition_id_id )
+(SELECT adverse_condition , timestamp_created , is_void , sync_id , adverse_condition_id_id FROM
+    dblink('dbname=cpims_dcs', 'select adverse_condition , timestamp_created , is_void , sync_id , adverse_condition_id_id FROM ovc_adverseevents_other_followup')
+AS t( adverse_condition varchar , timestamp_created date , is_void bool , sync_id uuid , adverse_condition_id_id uuid )
+
+);
+
+----working fine
+
+
+
+
+
+
+
 
 
 
