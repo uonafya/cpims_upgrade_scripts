4bdb43e56522994607308e86b8a3ac717106e720
Felix Ongati <fegati02@gmail.com>
2020-03-06 15:20:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 echo "< 002 (:) Merge queries started ..."
 
 # DB prep
-sh ./001_db_prep.sh
+psql -h localhost -d cpims -U cpimsdbuser -a -w -f all_foreign_keys_unicorn_ever_since_amen.sql
+psql -h localhost -d cpims -U cpimsdbuser -a -w -f reg_org_unit/reg_org_unit_update_ids.sql
+psql -h localhost -d cpims -U cpimsdbuser -a -w -f auth_user/alter_auth_user.sql
 
 echo "< 002 (a): import DCS reg_persons ..."
 psql -h localhost -d cpims -U cpimsdbuser -a -w -f append_data/append_reg_person_01.sql
