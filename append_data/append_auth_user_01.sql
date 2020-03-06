b48237f00556731a5831697c0005394e52d804f7
rosaga <richardjohnzranger@gmail.com>
2020-03-06 16:27:50 +0300

    Fix on auth user

--- append_data/append_auth_user_01.sql
+++ append_data/append_auth_user_01.sql
@@ -1,7 +1,7 @@
 --------------auth_user--------
 
 INSERT INTO auth_user (id, "password", last_login, is_superuser, "role", username, is_staff, is_active, timestamp_created, timestamp_updated, password_changed_timestamp, reg_person_id )
-(SELECT id, "password", last_login, is_superuser, "role", CONCAT (username,'_01',NULL) as username, is_staff, is_active, timestamp_created, timestamp_updated, password_changed_timestamp, reg_person_id  FROM
+(SELECT id, "password", last_login, is_superuser, "role", CONCAT (username,''_01'',NULL) as username, is_staff, is_active, timestamp_created, timestamp_updated, password_changed_timestamp, reg_person_id  FROM
     dblink('dbname=cmimdb', 'select id, "password", last_login, is_superuser, "role", CONCAT(username, ''_01'', NULL) as username, is_staff, is_active, timestamp_created, timestamp_updated, password_changed_timestamp, reg_person_id from auth_user where reg_person_id > 1')
 AS t( id int4, "password" varchar, last_login date, is_superuser bool, "role" varchar, username varchar, is_staff bool, is_active bool, timestamp_created date, timestamp_updated date, password_changed_timestamp date, reg_person_id int4));
 
